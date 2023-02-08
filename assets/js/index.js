b2fcbd2cfe1f0dc5929ea48f842d13f298c191bf
angeltorx <angelfitorres@gmail.com>
2023-02-07 20:07:42 -0500

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
-console.log("Entro index.js");
+//console.log("Entro index.js");
+
+const lista=[]
+
+//create
+const btnadd = (algo) => {
+    lista.push(algo);
+}
+
+// read
+const btnread=()=>{
+    lista.btnread
+}
+
+//update
+const  btnedit= (position, newValue) => {
+    lista.slice(position, 1, newValue);
+}
+
+//* delete
+const btndelete= (position)=>{
+    lista.splice(position, 1)
+}
+
